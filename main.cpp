a29795bc5eb415e0a665b151e4d4d0d4aafa1deb
TPPPP72 <906483498@qq.com>
2025-09-26 10:34:28 +0800

    修改样例代码

--- main.cpp
+++ main.cpp
@@ -1,52 +1,51 @@
 #include "lesson_test.h"
 #include <iostream>
+#include <memory>
 
 int main()
 {
     using namespace Lesson_Test;
 
     InteractTester tester;
-    tester.set_title("测试章节");
+    tester.set_title("测试章节"); // 可选
 
-    MCQ mcq;
-    CRP crp;
-    mcq.set_question("以下哪个选项能够正常打印Hello,World？");
-    mcq.add_option('A', R"(#include <iostream>
+    // 选择题样例
+    {
+        std::unique_ptr<MCQ> mcq = std::make_unique<MCQ>();
+        mcq->set_question("2^3 + 2^0 = ?");               // 设置题面 必须
+        mcq->add_option('A', "9");                        // 添加选项 必须
+        mcq->add_option('B', "8");                        // 添加选项 必须
+        mcq->add_option('C', "1");                        // 添加选项 必须
+        mcq->set_ans('A');                                // 设置答案 必须
+        mcq->set_hint("x^0 = 1");                         // 设置提示 可选
+        mcq->set_solution("2^3 = 8, 2^0 = 1, 8 + 1 = 9"); // 设置解析 可选
+        tester.add_question(*mcq);                        // 向测试器添加该题目 必须
+    }
 
-int main(){
-    std::cout << "Hello,World";
-})");
-    mcq.set_ans('A');
-    tester.add_question(mcq);
+    // 阅读代码题样例
+    {
+        std::unique_ptr<CRP> crp = std::make_unique<CRP>();
+        crp->set_origin_code(R"(#include <iostream>
 
-    crp.set_origin_code(R"(#include <stdio.h>
-
-int main(){
-    printf("Hello\n");
-    printf("World");
-})");
-    crp.set_real_output([]() { std::cout << "Hello\nWorld"; });
-    tester.add_question(crp);
-
-    crp.set_origin_code(R"(#include <stdio.h>
-
-int main(){
+int main()
+{
     int a, b;
-    scanf("%d%d", &a, &b);
-    printf("%d", a + b);
-})");
-    crp.set_input(R"(2 3)");
-    crp.set_real_output([]() { std::cout << "5"; });
-    tester.add_question(crp);
+    std::cin >> a >> b;
+    std::cout << a + b;
+})"); // 设置源代码 必须
 
-    MCQ *m = new MCQ();
-    m->set_question("哈基米叮咚鸡");
-    m->add_option('A', "你说得对但是");
-    m->add_option('B', "胖宝宝");
-    m->add_option('C', "说的道理");
-    m->set_ans('B');
-    tester.add_question(*m);
-    delete m;
+        // 方案一
+        crp->set_real_output("5"); // 设置真实输出 必须
+        // 方案二
+        crp->set_real_output([]() {
+            int a = 2, b = 3;
+            std::cout << a + b;
+        });                                                             // 设置真实输出 必须
+        crp->set_input("2 3");                                          // 设置输入 可选
+        crp->set_hint("cin 可以从控制台读入数据");                      // 设置提示 可选
+        crp->set_solution("输入 2 3 ，即 a = 2， b = 3，显然答案为 5"); // 设置解析 可选
+        tester.add_question(*crp);                                      // 向测试器添加该题目 必须
+    }
 
-    tester.test();
+    tester.test(); // 开始测试
 }
